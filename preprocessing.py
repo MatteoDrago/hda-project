bc60b4a03e90d7d16de3f434de41d7ea62a68982
RicLincio <37301469+RicLincio@users.noreply.github.com>
2018-07-06 12:17:56 +0200

    processing notebook

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,14 @@
 import numpy as np
 
 root = "./OpportunityUCIDataset/dataset/S"
-for sub in [1,2,3,4]:
-    for sess in [1,2,3,4,5,6]:
+for sub in [1]:#[1,2,3,4]:
+    for sess in [1]:#[1,2,3,4,5,6]:
         if sess < 6:
             filename = root + str(sub) + "-ADL" + str(sess) + ".dat"
             print("importing from: ", filename)
-            data = np.genfromtxt('sample.dat',
-                                 skip_header=1,
-                                 skip_footer=1,
-                                 names=True,
-                                 dtype=None,
-                                 delimiter=' ')
-            print(data)
+            data = np.genfromtxt(filename,
+                                 usecols=([1:46]))
+            print(data.shape)
 
         else:
             filename = root + str(sub) + "-Drill.dat"
