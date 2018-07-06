c105b0e928e5e3f5d8355957c5634edc0e16f518
DrMello <matteo.drago1994@gmail.com>
2018-07-06 23:21:19 +0200

    tested new preproc solution

--- preprocessing_v2.m
+++ preprocessing_v2.m
@@ -11,15 +11,22 @@ for subject = 1:4
         
         data = load(filename);
         features = data(:,[2:46 51:59 64:72 77:85 90:98 103:134]);
-        labels = data(:,[244:250]);
+        labels = data(:,244:250);
         
         features(labels(:,1)==0,:) = [];
         labels(labels(:,1)==0,:) = [];
+       
         filled_features = fillmissing(features,'spline');
-
-        output = "prep\A_nonzero\S" + int2str(subject) + "-ADL" + int2str(session) + ".mat";
+        reduced_features = zeros(size(filled_features,1),58);
+        j = 1;
+        for i=1:3:81-3
+            reduced_features(:,j) = sqrt(filled_features(:,i).^2 + filled_features(:,i+1).^2 + filled_features(:,i+2).^2);
+            j = j + 1;
+        end
+        reduced_features(:,j:end) = filled_features(:,82:end);
+        output = "prep\acc_magni\S" + int2str(subject) + "-ADL" + int2str(session) + ".mat";
         
-        save(output, 'filled_features', 'labels')
+        save(output, 'reduced_features', 'labels')
     end
 end
 
@@ -35,7 +42,16 @@ for subject = 1:4
     labels(labels(:,1)==0,:) = [];
     
     filled_features = fillmissing(features,'spline');
-    output = "prep\A_nonzero\S" + int2str(subject) + "-Drill.mat";
-    save(output, 'filled_features', 'labels')
+    reduced_features = zeros(size(filled_features,1),58);
+    
+    j = 1;
+    for i=1:3:81-3
+        reduced_features(:,j) = sqrt(filled_features(:,i).^2 + filled_features(:,i+1).^2 + filled_features(:,i+2).^2);
+        j = j + 1;
+    end
+    
+    output = "prep\acc_magni\S" + int2str(subject) + "-Drill.mat";
+    
+    save(output, 'reduced_features', 'labels')
 end
 
