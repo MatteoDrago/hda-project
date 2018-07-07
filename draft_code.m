382988ddb6dbd91609cc1377ded83b46ca9b9b87
DrMello <matteo.drago1994@gmail.com>
2018-07-07 17:12:55 +0200

    started jupyter for task A

--- draft_code.m
+++ draft_code.m
@@ -9,6 +9,19 @@ data = load(".\OpportunityUCIDataset\dataset\S1-ADL5.dat");
 features = data(:,[2:46 51:59 64:72 77:85 90:98 103:134]);
 labels = data(:,[244:250]);
 
+temp = isnan(features(:,1));
+H = zeros(size(temp));
+count = 0;
+for idx = 1:numel(temp)
+  if temp(idx)
+    count = count + 1;
+  else
+    count = 0;
+  end
+  H(idx) = count;
+end
+
+
 filled_features = fillmissing(features,'spline');
 
 idx = zeros(2,7);
