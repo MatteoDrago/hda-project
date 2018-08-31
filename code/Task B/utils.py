14dd37ee7efaad2af5015c1bd0b0cb660f8176f9
RicLincio <37301469+RicLincio@users.noreply.github.com>
2018-08-31 10:50:10 +0200

    models

--- code/Task B/utils.py
+++ code/Task B/utils.py
@@ -1,12 +1,56 @@
+import keras.backend as K
+from sklearn.metrics import f1_score
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+
+
+def AUC(y_true, y_pred, classes):
+    """ Compute the Area Under the Curve of ROC metric. """
+
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(classes):
+        fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    return roc_auc
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
 # In the following cell, we make use of some functions of Keras which have been removed,
 # but of which the code is still available at 
 # https://github.com/keras-team/keras/commit/a56b1a55182acf061b1eb2e2c86b48193a0e88f7.
 # These are used to evaulate the f1 score during training on batches of data:
 # this is only an approximation though, which is the reason why they have been removed.
 
-import keras.backend as K
-from sklearn.metrics import f1_score
-
+########################################################################################
 
 def precision(y_true, y_pred): 
     """Precision metric.
@@ -76,3 +120,5 @@ def fmeasure(y_true, y_pred):
     """ 
 
     return fbeta_score(y_true, y_pred, beta=1)
+
+########################################################################################
